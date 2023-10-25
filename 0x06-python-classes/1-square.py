04b82e0db5879ba7eaf538607055ae9b42038364
Blessing <nejo.blessing@yahoo.com>
2023-10-24 21:21:43 -0700

    python object and classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size):
+        """constructor.
+
+        Args:
+            size: length of a side of the square.
+        """
+        self.__size = size
+
+
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+0x06 Python-Object and Classes Project
