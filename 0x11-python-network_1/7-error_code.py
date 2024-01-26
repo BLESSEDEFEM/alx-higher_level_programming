168f33317fd0b25457baa25d1965fca014ff8992
Blessing <nejo.blessing@yahoo.com>
2024-01-26 10:21:49 -0800

    python network 1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """ This sends a request to a given URL and displays the response body.
 Usage: ./7-error_code.py <URL>
+  - Handles HTTP errors
 """
 import sys
-import inquests
+import requests
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    r = inquests.get(url)
+    r = requests.get(url)
     if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
     else:
